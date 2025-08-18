3b07693f90b881c2bbad2704635ad50431224bff
JamesWallimperial <jfw124@ic.ac.uk>
2025-08-18 01:28:07 +0100

    fixed issues with npm dev for displaying UI on browser

--- src/ui/web/src/main.tsx
+++ src/ui/web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./global.css";
+import "./base.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root")!);
